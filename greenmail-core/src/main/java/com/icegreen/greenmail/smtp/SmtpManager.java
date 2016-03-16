72b436592b8971f5ade12ff15b2dc5463d15b1f9
Charly GAULAY <cgaulay.externe@pagesjaunes.fr>
2016-03-16 16:15:41 +0100

    [TECH] Ajout d'une méthode pour stocker l'email créé en BDD pour autocomplétion dashboard

--- greenmail-core/src/main/java/com/icegreen/greenmail/smtp/SmtpManager.java
+++ greenmail-core/src/main/java/com/icegreen/greenmail/smtp/SmtpManager.java
@@ -6,52 +6,64 @@
  */
 package com.icegreen.greenmail.smtp;
 
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.icegreen.greenmail.imap.ImapHostManager;
 import com.icegreen.greenmail.mail.MailAddress;
 import com.icegreen.greenmail.mail.MovingMessage;
 import com.icegreen.greenmail.user.GreenMailUser;
 import com.icegreen.greenmail.user.UserManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 
 public class SmtpManager {
     protected static final Logger log = LoggerFactory.getLogger(SmtpManager.class);
 
     Incoming _incomingQueue;
+
     UserManager userManager;
-    private ImapHostManager imapHostManager;
+
+    private final ImapHostManager imapHostManager;
+
+    private static final String MONGO_DB_DATABASE = "OUTILLAGE";
+
+    private static final String MONGO_DB_COLLECTION = "utilisateurs_greenmail";
+
+    private static final String MONGO_DB_IP = "cdmongotools01t.bbo1t.local:27017";
+
     List<WaitObject> notifyList;
 
-    public SmtpManager(ImapHostManager imapHostManager, UserManager userManager) {
+    public SmtpManager(final ImapHostManager imapHostManager, final UserManager userManager) {
         this.imapHostManager = imapHostManager;
         this.userManager = userManager;
         _incomingQueue = new Incoming();
         notifyList = Collections.synchronizedList(new ArrayList<WaitObject>());
     }
 
-
-    public String checkSender(SmtpState state, MailAddress sender) {
+    public String checkSender(final SmtpState state, final MailAddress sender) {
         //always ok
         return null;
     }
 
-    public String checkRecipient(SmtpState state, MailAddress rcpt) {
+    public String checkRecipient(final SmtpState state, final MailAddress rcpt) {
         // todo?
 //        MailAddress sender = state.getMessage().getReturnPath();
         return null;
     }
 
-    public String checkData(SmtpState state) {
+    public String checkData(final SmtpState state) {
         return null;
     }
 
-    public synchronized void send(SmtpState state) {
+    public synchronized void send(final SmtpState state) {
         _incomingQueue.enqueue(state.getMessage());
         for (WaitObject o : notifyList) {
             synchronized (o) {
@@ -63,7 +75,7 @@ public class SmtpManager {
     /**
      * @return null if no need to wait. Otherwise caller must call wait() on the returned object
      */
-    public synchronized WaitObject createAndAddNewWaitObject(int emailCount) {
+    public synchronized WaitObject createAndAddNewWaitObject(final int emailCount) {
         final int existingCount = imapHostManager.getAllMessages().size();
         if (existingCount >= emailCount) {
             return null;
@@ -86,9 +98,10 @@ public class SmtpManager {
      */
     public static class WaitObject {
         private boolean arrived = false;
+
         private int emailCount;
 
-        public WaitObject(int emailCount) {
+        public WaitObject(final int emailCount) {
             this.emailCount = emailCount;
         }
 
@@ -116,14 +129,14 @@ public class SmtpManager {
     }
 
     private class Incoming {
-        public void enqueue(MovingMessage msg) {
+        public void enqueue(final MovingMessage msg) {
             for (MailAddress address : msg.getToAddresses()) {
                 handle(msg, address);
             }
 
         }
 
-        private void handle(MovingMessage msg, MailAddress mailAddress) {
+        private void handle(final MovingMessage msg, final MailAddress mailAddress) {
             try {
                 GreenMailUser user = userManager.getUserByEmail(mailAddress.getEmail());
                 if (null == user) {
@@ -132,6 +145,8 @@ public class SmtpManager {
                     String password = mailAddress.getEmail();
                     user = userManager.createUser(email, login, password);
                     log.info("Created user login {} for address {} with password {} because it didn't exist before.", login, email, password);
+                    saveUserMail(email);
+                    log.info("Saved user login {} in BDD", email);
                 }
 
                 user.deliver(msg);
@@ -141,6 +156,22 @@ public class SmtpManager {
             }
 
             msg.releaseContent();
+        }
+
+        /** Sauvegarde du mail de l'utilisateur pour autocomplétion dashboard */
+        private void saveUserMail(final String pMail) {
+            MongoClient client;
+            try {
+                client = new MongoClient(MONGO_DB_IP);
+                DB db = client.getDB(MONGO_DB_DATABASE);
+                DBCollection collection = db.getCollection(MONGO_DB_COLLECTION);
+                BasicDBObject userDb = new BasicDBObject();
+                userDb.put("mail", pMail);
+                //on update avec upsert à true pour créer si l'entrée n'existe pas. Ca évite les doublons.
+                collection.update(userDb, userDb, true, false);
+            } catch (UnknownHostException e) {
+                throw new RuntimeException("Erreur lors du stockage de l'adresse mail de l'utilisateur : BDD injoignable ", e);
+            }
         }
     }
 }
